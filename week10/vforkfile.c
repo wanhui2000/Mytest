d573ed6fb43c06ea21312c20b9f53fa334cb06e2
wan <1362098851@qq.com>
2019-12-22 22:40:46 -0800

    tenth

---
+++ week10/vforkfile.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/types.h>
+
+
+int main(){
+ int i;
+ FILE *fp;
+ char buf[100];
+ fp=fopen("/home/wan/Desktop/week10/filetest2.txt","w");
+ pid_t pid;
+ pid = vfork();
+ if(pid<0)
+ {
+ 	perror("fork defalut!");
+	exit;
+ }
+ else if(pid == 0){
+
+	
+ printf("input write1 words :");
+ scanf("%s",buf);	
+ getchar();
+ if(fp==NULL) 
+ {
+	printf("\nopen file error");
+	getchar();
+ }
+ fwrite(buf, strlen(buf), 1, fp);
+ fclose(fp);
+	return 0;
+ }
+ else{
+ sleep(10);
+ printf("input write2 words :");
+ scanf("%s",buf);	
+ getchar();
+ if(fp==NULL) 
+ {
+	printf("\nopen file error");
+	getchar();
+ }
+ fwrite(buf, strlen(buf), 1, fp);
+ fclose(fp);
+	return 0;
+ }
+
+
+}
